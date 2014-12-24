0919819f7613197c36bba72fb8006059b694b985
Chinseok Lee <allieuslee@gmail.com>
2014-12-25 03:41:31 +0900

    SimpleCursorAdapter 의 deprecated flag (FLAG_AUTO_REQUERY) 제거

--- app/src/main/java/com/sunkist/photoboard/ContactsActivity.java
+++ app/src/main/java/com/sunkist/photoboard/ContactsActivity.java
@@ -7,6 +7,7 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
@@ -21,12 +22,17 @@ public class ContactsActivity extends ActionBarActivity implements LoaderManager
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
 
-        // SimpleCursorAdapter, flags 가 없는 생성자는 api 11 에서 deprecated -> CursorLoader 를 추천
-        // http://developer.android.com/reference/android/widget/SimpleCursorAdapter.html
-        // 이유인즉, UI Thread 성능이슈로 인해 ANR 이 발생할 수도 있다는 것 !!!
         adapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1,
                 null, new String[] { ContactsContract.Contacts.DISPLAY_NAME },
-                new int[] { android.R.id.text1});
+                new int[] { android.R.id.text1},
+
+                // api 11 에서 deprecated
+                // http://developer.android.com/reference/android/widget/CursorAdapter.html#FLAG_AUTO_REQUERY
+                // UI Thread 성능이슈로 인해 ANR 이 발생할 수도 있기 때문.
+                // ??? : CursorLoad 와 CursorAdapter 를 같이 쓰는 경우 중복으로 리쿼리된다 ???
+                // CursorAdapter.FLAG_AUTO_REQUERY |
+
+                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
 
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adapter);
